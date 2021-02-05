f5955605e573797c37457f384e4e836236fc6547
John Olafenwa <john.olafenwa@microsoft.com>
2021-02-06 00:28:27 +0100

    removed scikit image dependency

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup,find_packages
 
 setup(name= "deepstack-sdk",
-      version='0.2.0',
+      version='0.2.1',
       description='DeepStack Python SDK',
       url="https://github.com/johnolafenwa/DeepStackPython",
       author='John Olafenwa and Moses Olafenwa',
       license='MIT',
       packages= find_packages(),
-      install_requires=['pillow','scikit-image','opencv-python','matplotlib',"requests","pytest","validators"],
+      install_requires=['pillow','opencv-python','matplotlib',"requests","pytest","validators"],
       zip_safe=False,
       classifiers=[
         "Programming Language :: Python :: 3",
