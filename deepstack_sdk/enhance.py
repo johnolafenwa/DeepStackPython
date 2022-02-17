cc7ac070848edd3546ab15b046623cc636424d6f
Alimat sadia <anikesadia01@gmail.com>
2022-02-17 14:39:39 +0100

    Creating enhance api

---
+++ deepstack_sdk/enhance.py
@@ -0,0 +1,62 @@
+from PIL import Image
+from .config import ServerConfig
+from .utils import cv2ToBytes, pilToBytes
+from .structs import EnhanceResponse
+import requests
+import os
+import validators
+import numpy
+import base64
+
+
+class Enhance(object):
+    def __init__(self, config: ServerConfig, name=None):
+        self.config = config
+        if name is None:
+            self.__endpoint = "/v1/vision/enhance"
+
+    def __process_image(self, image_data: bytes):
+        response = requests.post(self.config.server_url + self.__endpoint,
+                                 files={"image": image_data},
+                                 data={"api_key": self.config.api_key}
+                                 )
+
+        return response
+
+    # allow folder input, with exts param specifying comma separated exts
+    def enhanceObject(self, image, format="jpg", output=None):
+        if isinstance(image, str):
+            if os.path.isfile(image):
+                image_data = open(image, "rb").read()
+            elif validators.url(image):
+                image_data = requests.get(image).content
+            else:
+                raise Exception("file {} does not exist".format(image))
+        elif isinstance(image, numpy.ndarray):
+            image_data = cv2ToBytes(image, format=format)
+        elif isinstance(image, Image.Image):
+            image_data = pilToBytes(image, format=format)
+        elif isinstance(image, bytes):
+            image_data = image
+        else:
+            raise Exception("Unsupported input type: {}".format(type(image)))
+
+        response = self.__process_image(image_data)
+
+        if response.status_code == 200:
+            data = EnhanceResponse(response.json())
+            if output is not None:
+                enhance_base64 = data["base64"]
+                image4X_byte = base64.b64decode(enhance_base64)
+                with open(output, 'wb') as f_output:
+                    f_output.write(image4X_byte)
+
+            return data
+        elif response.status_code == 403:
+            raise Exception("The scene endpoint is not enabled on the DeepStack server")
+        elif response.status_code == 400:
+            raise Exception("Invalid image")
+        elif response.status_code == 500:
+            raise Exception("An error occured on the DeepStack server")
+        else:
+            raise Exception("Unknown error : {} occured".format(response.status_code))
---
+++ tests/enhance/test_enhance.py
@@ -0,0 +1,42 @@
+from deepstack_sdk import Enhance, ServerConfig, pilToBytes
+import os
+import cv2
+from PIL import Image
+
+DEEPSTACK_URL = os.getenv("TEST_DEEPSTACK_URL")
+IMAGES_DIR = os.getenv("TEST_DATA_DIR")
+config = ServerConfig(DEEPSTACK_URL)
+
+
+def test_enhance_file():
+    enhance = Enhance(config)
+    res = enhance.enhanceObject(os.path.join(IMAGES_DIR, "sky.jpg"))
+    assert len(res) == 3
+
+
+def test_enhance_url():
+    enhance = Enhance(config)
+    res = enhance.enhanceObject("https://docs.deepstack.cc/_images/sky.jpg")
+    assert len(res) == 3
+
+
+def test_enhance_cv2():
+    enhance = Enhance(config)
+    img = cv2.imread(os.path.join(IMAGES_DIR, "sky.jpg"))
+    res = enhance.enhanceObject(img)
+    assert len(res) == 3
+
+
+def test_enhance_pil():
+    enhance = Enhance(config)
+    img = Image.open(os.path.join(IMAGES_DIR, "sky.jpg"))
+    res = enhance.enhanceObject(img)
+    assert len(res) == 3
+
+
+def test_enhance_bytes():
+    enhance = Enhance(config)
+    img = Image.open(os.path.join(IMAGES_DIR, "sky.jpg"))
+    img_data = pilToBytes(img)
+    res = enhance.enhanceObject(img_data)
+    assert len(res) == 3
