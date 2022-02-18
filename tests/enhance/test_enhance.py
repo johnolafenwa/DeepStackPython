9b2a2342f3147d33ba3a4f15ed091059b6065b0c
Alimat sadia <anikesadia01@gmail.com>
2022-02-18 12:43:46 +0100

    Testing enhance4

--- tests/enhance/test_enhance.py
+++ tests/enhance/test_enhance.py
@@ -11,31 +11,31 @@ config = ServerConfig(DEEPSTACK_URL)
 def test_enhance_file():
     enhance = Enhance(config)
     res = enhance.enhanceObject(os.path.join(IMAGES_DIR, "disney.png"))
-    assert res["width"] == 172
-    assert res["height"] == 172
+    assert res.width == 172
+    assert res.height == 172
 
 
 def test_enhance_cv2():
     enhance = Enhance(config)
     img = cv2.imread(os.path.join(IMAGES_DIR, "disney.png"))
     res = enhance.enhanceObject(img)
-    assert res["width"] == 172
-    assert res["height"] == 172
+    assert res.width == 172
+    assert res.height == 172
 
 
 def test_enhance_url():
     enhance = Enhance(config)
     res = enhance.enhanceObject("https://docs.deepstack.cc/_images/sky.jpg")
-    assert res["width"] == 1840
-    assert res["height"] == 1036
+    assert res.width == 1840
+    assert res.height == 1036
 
 
 def test_enhance_pil():
     enhance = Enhance(config)
     img = Image.open(os.path.join(IMAGES_DIR, "disney.png"))
     res = enhance.enhanceObject(img)
-    assert res["width"] == 172
-    assert res["height"] == 172
+    assert res.width == 172
+    assert res.height == 172
 
 
 def test_enhance_bytes():
@@ -43,5 +43,5 @@ def test_enhance_bytes():
     img = Image.open(os.path.join(IMAGES_DIR, "disney.png"))
     img_data = pilToBytes(img)
     res = enhance.enhanceObject(img_data)
-    assert res["width"] == 172
-    assert res["height"] == 172
+    assert res.width == 172
+    assert res.height == 172
